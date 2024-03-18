c9f0713f6b0b1ae5ccd7d2da5a23e13544ed9261
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-03-18 10:42:39 -0400

    removed

--- app/src/pdf-utils/start.ts
+++ app/src/pdf-utils/start.ts
@@ -1,11 +1,8 @@
 import { fetchUtil } from "./fetchUtil";
 
 export const start = async (tool: string) => {
-    try {
-        const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${tool}`)
-        console.log(res);
-        return res
-  } catch (err) {
-    console.error(err)
-  }
+    const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${tool}`, {
+            
+        })
+        
 };
