cc4031808eb58d83383c614efb1afefc0df14ad1
zaehicks <isaiahhickerson@gmail.com>
2024-03-18 14:22:59 -0400

    uploadFiles function completed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
-import { getAuthToken, start } from "./pdf-utils/pdfFunctions";
-import { publicKey } from "../keys.ts";
-import "./App.css";
+
 
 export default function App() {
   
-  return <div></div>;
+
+  return (
+    <div>
+      
+      
+    </div>
+  );
 }
--- app/src/pdf-utils/pdfFunctions.ts
+++ app/src/pdf-utils/pdfFunctions.ts
@@ -15,9 +15,37 @@ export const start = async (tool: string, bearerToken: string) => {
   const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${tool}`, {
     method: "GET",
     headers: {
-        "Content-Type": "applications/json",
-        "Authorization": `Bearer ${bearerToken}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${bearerToken}`,
     },
   });
-    return res;
+  return res;
+};
+
+export const uploadFiles = async (
+  serverUrl: string,
+  taskId: string,
+  formData: FormData,
+  bearerToken: string,
+
+) => {
+  formData.append("task", taskId);
+
+
+  const response = await fetch(`https://${serverUrl}/v1/upload`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to upload file to iLovePDF: ${response.statusText}`
+    );
+  }
+
+  const data = await response.json();
+  return data;
 };
