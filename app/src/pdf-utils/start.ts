59ed974c12a4782a4dec52a18205dd429f354718
daniel-figg <daniel.figg05@gmail.com>
2024-03-18 10:54:52 -0400

    Remove unused start function

--- app/src/pdf-utils/start.ts
+++
@@ -1,8 +0,0 @@
-import { fetchUtil } from "./fetchUtil";
-
-export const start = async (tool: string) => {
-    const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${tool}`, {
-            
-        })
-        
-};
