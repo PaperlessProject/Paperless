c2b2c35843b66e7085083c8fec3ea6252f8499e8
daniel-figg <daniel.figg05@gmail.com>
2024-03-18 12:00:10 -0400

    Refactor PDF functions and add new functionality

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,41 @@
-
-import "./App.css";
+import { useEffect } from 'react';
+import {
+  getAuthToken,
+  start,
+  upload,
+  process,
+  download,
+} from './pdf-utils/pdfFunctions';
+import { publicKey } from '../keys.ts';
+import './App.css';
 
 export default function App() {
+  const file = new File([''], 'test.pdf', { type: 'application/pdf' });
+  useEffect(() => {
+    const runPdfFunctions = async () => {
+      const bearerToken = await getAuthToken(publicKey);
 
+      const startResponse = await start('compress', bearerToken);
+      const { server, task } = startResponse;
+      console.log('startResponse', startResponse);
+
+      const uploadResponse = await upload(server, task, file, bearerToken);
+      console.log('uploadResponse', uploadResponse);
+
+      const processResponse = await process(
+        server,
+        task,
+        'compress',
+        file,
+        bearerToken
+      );
+      console.log('processResponse', processResponse);
+
+      const downloadResponse = await download(server, task, bearerToken);
+      console.log('downloadResponse', downloadResponse);
+    };
+
+    runPdfFunctions();
+  }, []);
   return <div></div>;
 }
--- app/src/pdf-utils/pdfFunctions.ts
+++ app/src/pdf-utils/pdfFunctions.ts
@@ -8,50 +8,64 @@ export const getAuthToken = async (publicKey: string) => {
     },
     body: JSON.stringify({ public_key: publicKey }),
   });
-  return res;
+  return res.token;
 };
 
-export const startTask = async (authToken: string, task: string) => {
-  const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${task}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return res;
-};
-
-export const uploadFile = async (authToken: string, file: File) => {
-  const formData = new FormData();
-  formData.append('file', file);
-
-  const res = await fetchUtil('https://api.ilovepdf.com/v1/upload', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-    body: formData,
-  });
-  return res;
-};
-
-export const processTask = async (authToken: string, task: string) => {
-  const res = await fetchUtil(`https://api.ilovepdf.com/v1/process/${task}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return res;
-};
-
-export const downloadFile = async (authToken: string, task: string) => {
-  const res = await fetchUtil(`https://api.ilovepdf.com/v1/download/${task}`, {
+export const start = async (tool: string, bearerToken: string) => {
+  const res = await fetchUtil(`https://api.ilovepdf.com/v1/start/${tool}`, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${authToken}`,
+      'Content-Type': 'applications/json',
+      Authorization: `Bearer ${bearerToken}`,
+    },
+  });
+  return res;
+};
+
+export const upload = async (
+  server: string,
+  task: string,
+  file: File,
+  bearerToken: string
+) => {
+  const res = await fetch(`https://${server}/v1/upload/`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    body: JSON.stringify({ task, file }),
+  });
+  return res;
+};
+
+export const process = async (
+  server: string,
+  task: string,
+  tool: string,
+  files: File,
+  bearerToken: string
+) => {
+  const res = await fetchUtil(`https://${server}/v1/process`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    body: JSON.stringify({ task, tool, files }),
+  });
+  return res;
+};
+
+export const download = async (
+  server: string,
+  task: string,
+  bearerToken: string
+) => {
+  const res = await fetchUtil(`https://${server}/v1/download/${task}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${bearerToken}`,
     },
   });
   return res;
